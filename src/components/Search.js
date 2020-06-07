e56d2bae83a7fedfa58cfe04564fde7f18ffc7de
Ramunas-Bkr <54622805+Ramunas-Bkr@users.noreply.github.com>
2020-06-07 11:34:27 +0300

    github state. Main page

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,25 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { AlertContext } from '../context/alert/alertContext'
+import { GithubContext } from '../context/github/githubContext'
 
 export const Search = () => {
-
-    const {show} = useContext(AlertContext)
+    const [value, setValue] = useState('')
+    const alert = useContext(AlertContext)
+    const gitHub = useContext(GithubContext)
 
     const onSubmit = event => {
-        if (event.key === 'Enter') {
-            show('This is alert!')
+        if (event.key !== 'Enter') {
+            return null
+        }
+
+        gitHub.clearUsers()
+
+        if (value.trim()) {
+            alert.hide()
+            gitHub.search(value.trim())
+            
+        } else {
+            alert.show('Enter user data')
         }
     }
 
@@ -18,6 +30,8 @@ export const Search = () => {
                 type="text"
                 placeholder="Enter your name here"
                 onKeyPress={onSubmit}
+                value={value}
+                onChange={event => setValue(event.target.value)}
             />
         </div>
     )
